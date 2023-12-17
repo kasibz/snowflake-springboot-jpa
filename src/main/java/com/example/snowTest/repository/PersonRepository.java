9f528528f3e6196c516442b43f2d3acc172838d7
kasib <kasibzxc@gmail.com>
2023-12-17 12:22:04 -0500

    commented out unused native queries, used ORM instead

--- src/main/java/com/example/snowTest/repository/PersonRepository.java
+++ src/main/java/com/example/snowTest/repository/PersonRepository.java
@@ -2,8 +2,6 @@ package com.example.snowTest.repository;
 
 import com.example.snowTest.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
